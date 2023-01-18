19998502ea321c09b6bea35c1f6df4bb8cf85e93
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-18 19:52:30 -0300

    refactor: move prisma instance to `lib/prisma.ts`

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 const PORT = 3333;
 
