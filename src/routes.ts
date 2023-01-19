fbf5865b1fba37e436e1acc70cc5a7c8006d05bd
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-19 14:40:40 -0300

    feat: add `/day` route

--- src/routes.ts
+++ src/routes.ts
@@ -27,4 +27,41 @@ export async function appRoutes(app: FastifyInstance) {
 
     console.log(`✏️  Habit "${title}" created`);
   });
+
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const parsedDate = dayjs(date).startOf('day');
+    const weekDay = parsedDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((habit) => habit.habit_id);
+
+    return { possibleHabits, completedHabits };
+  });
 }
